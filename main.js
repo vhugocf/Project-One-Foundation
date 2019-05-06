3436f328f00e8b404e429ca246844a574920951a
victor hugo <vhugocf@hotmail.com>
2019-05-05 23:25:14 -0300

    parte domingo final noite

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('.menu-toggler').on('click', function(){
+$(document).ready(function (){
+    $('.menu-toggler').on('click', function (){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
